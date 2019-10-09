d9fa57161f16eaeb435332e1c35649a26f1f58d7
David Royer <droyer01@gmail.com>
2019-10-09 17:40:07 -0400

    update

---
+++ src/components/ProgramsTable.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="q-pa-md table-wrapper">
+    <q-table
+      title="UWA Programs"
+      :data="tableData"
+      :columns="columns"
+      row-key="id"
+      :filter="filter"
+    >
+      <template v-slot:top-right>
+        <q-input
+          borderless
+          dense
+          debounce="300"
+          v-model="filter"
+          placeholder="Search"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+    </q-table>
+  </div>
+</template>
+
+<script>
+import uwaData from "../data/uwa.json";
+export default {
+  // name: 'ComponentName',
+  data() {
+    return {
+      filter: "",
+
+      columns: [
+        {
+          name: "name",
+          label: "Program Name",
+          field: "name",
+          sortable: true,
+          align: "left"
+        },
+        {
+          name: "level",
+          label: "Level",
+          field: "level",
+          sortable: true,
+          align: "left"
+        },
+        { name: "id", label: "ID", field: "id", align: "left" }
+      ],
+      tableData: uwaData
+    };
+  }
+};
+</script>
+
+<style>
+.q-table__container {
+  /* max-width: 85%; */
+  margin-left: auto;
+  margin-right: auto;
+}
+.table-wrapper {
+  width: 100%;
+  max-width: 1250px;
+}
+</style>
--- src/pages/Programs.vue
+++ src/pages/Programs.vue
@@ -1,19 +1,16 @@
 <template>
   <q-page class="flex flex-center">
-    <DataTable />
-    <!-- <q-btn color="primary" icon="check" label="OK" @click="onClick" /> -->
+    <ProgramsTable />
   </q-page>
 </template>
 
 <script>
 import DataTable from "components/DataTable";
+import ProgramsTable from "components/ProgramsTable";
+
 export default {
   name: "PagePrograms",
-  components: { DataTable },
-  methods: {
-    onClick() {
-      alert("Clicked!");
-    }
-  }
+  // eslint-disable-next-line vue/no-unused-components
+  components: { DataTable, ProgramsTable }
 };
 </script>
